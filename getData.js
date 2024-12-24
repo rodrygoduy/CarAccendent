b5346abe43c1ce131aa94b50c835963279f25acb
Duy Nguyen <160684531+rodrygoduy@users.noreply.github.com>
2024-12-24 19:49:12 +0700

    U

--- getData.js
+++ getData.js
@@ -1,13 +1,10 @@
-// Import necessary modules
+
 const axios = require('axios');
 const { MongoClient } = require('mongodb');
-
-// Define constants
 const COOKIE = "sb=yY8PZ2hJgS4IYsJYftGlwNxP; datr=yY8PZ8-6cScq5TTemNkDKhaE; ps_l=1; ps_n=1; c_user=100070448054752; xs=20%3AOMqR2kLr7XWf1A%3A2%3A1734707777%3A-1%3A6427; fr=1OvTlq5o8l4fR6qPP.AWV7CQ7l7EjbRBmHSjfiojhREiE.BnZYgy..AAA.0.0.BnZZb5.AWX80vGQeEQ; dpr=1.125; presence=C%7B%22t3%22%3A%5B%5D%2C%22utc3%22%3A1734711044099%2C%22v%22%3A1%7D; wd=1126x811";
 const URL = "https://www.facebook.com/api/graphql/";
 const INITIAL_PAYLOAD = `__user=100070448054752&__a=1&__req=3n&__hs=20077.HYP%3Acomet_pkg.2.1.0.2.1&dpr=1&__ccg=EXCELLENT&__rev=1019029190&__s=rpgwby%3A7hbkuy%3Arvsvcx&__hsi=7450513179139818351&__dyn=7xeXzWK2l2u7Ung569yaxG4Qih0noeEb8nwgUaofVUKbgS3qi7UK361twYwJyEiwsobo6u3y4o27wywn82nwb-q7oc81EEbbwto88422y11wBz822wtU4a3a4oaEnxO0Bo4O2-2l2UtwxwhU31wiE567Udo5qfK0zEkxe2GexeeDwkUtxGm2SU4i5oe8cEW4-5pUfEdbwxwhFVovUaU6a1TxW2-VEbUGdG0HE88cA0z8c84p1e4UK2K2WEjxK2B08-269wkopg6C13xe3a3Gfw-Kufxa3mUqwjVqwLwHwGwbu5E&__csr=g8s7IfN4kx4cMBb1n5mxs_NAQAr9SB4ccsOZ9hkzkAWPN2lHGGmnsytiWuoyAWEJtsJdbvh6HQ9Bh4jHvFnl9SlGRHqUyAmLpqUCh2pFUHmERbpdemRJauV4jFLh9eFWrABV5UJyFohy44pp7BDKaLhGjAvBHCGqdy9WHy89UOiHhpuiVozy8WmfGbzVZ3o-mmuQ4pV9em4ojxWUhyaAy8nXxeU98a9ucx2GDV8S49aADDxiUizQaAGiiEjxiUhDQ5EWm9DK6U8UG5F8KmeUgVEizpU4CawLzQ8AAG58Kcggxe3e2y6Vp8K3K5EbA4poowxyUaAczqSGwXDxC1rxeu9wwwzwFBwl9F41rzqye221oyo3pw6rAwoE5t04RwaOaCzU4K0RouyHzUqo1QS0ra1pwo46Oyki9xB4wt815k1kwywNJ90GwjA6U7e04jliLFxK8m4sHFk0MA02se3Wfg1etQ0ri2O0oO8xW08Gwf2041o0vpx500rEUmui-03HS079E1ho4ugCb4w2qU22yVU1r9UC3d0deqi0GECq0Doeo13oG3WOwmEyu0jW0CU5y2a0hC1Yw4US1Xw8W0Z8CbBodUC0qeu0vq0xA0xV2N04gweoaU2DxS08pw1hq0WElBK4o99Uy2a0H8dbgy36zh2xdVD4PHe8G8Q2q7U7GWig1jEO0va264o8U6O1D86o25w1bC02rS-0gB021UdoaQpi90Jxe0XLG3u0Go1oUC0pq2Kfo3Fwlo0ye07aqxj2cb042w18W7o3gwho6C&__comet_req=15&fb_dtsg=NAcOeXHrVuLhqT7H0jkuGoIIQ2Y6eSrq31uA5-iMs7m6mTCiNwaSccQ%3A20%3A1734707777&jazoest=25454&lsd=bxoyItvy_mI7dnM-oKF4_W&__spin_r=1019029190&__spin_b=trunk&__spin_t=1734707779&fb_api_caller_class=RelayModern&fb_api_req_friendly_name=GroupsCometFeedRegularStoriesPaginationQuery&variables=%7B%22count%22%3A3%2C%22cursor%22%3A%22Cg8TZXhpc3RpbmdfdW5pdF9jb3VudAICDwtyZWFsX2N1cnNvcg%2BfQVFIUmlITThzTnFVcjJoV0tpdjFDTEFJZkFwMk8xRWQ1TWtjeHZ0UGVEbmQyYlBEWnJvWGJYQ0tWVldsM0V0ZXp1RWRxZWRxcFhZa1QybFlzRl9yUWZ3SEVBOmV5SXdJam94TnpNME56QTRNVEExTENJeElqbzNOamd5TENJeklqb3dMQ0kwSWpveExDSTFJam95TENJMklqb3RNWDA9DxNoZWFkZXJfZ2xvYmFsX2NvdW50AgEPEm1haW5fZmVlZF9wb3NpdGlvbgICDw1mZWVkX29yZGVyaW5nDw1hdHRhY2hlZF90aW1lDxNpc19ldmVyZ3JlZW5fY3Vyc29yEQAPImlzX29mZmxpbmVfYWdncmVnYXRlZF9wb3N0c19jdXJzb3IRAA8SZ3JvdXBfZmVlZF92ZXJzaW9uDwJWMg8QZGVtb3RlZF9wb3N0X2lkcwoBAQ%3D%3D%22%2C%22feedLocation%22%3A%22GROUP%22%2C%22feedType%22%3A%22DISCUSSION%22%2C%22feedbackSource%22%3A0%2C%22focusCommentID%22%3Anull%2C%22privacySelectorRenderLocation%22%3A%22COMET_STREAM%22%2C%22renderLocation%22%3A%22group%22%2C%22scale%22%3A1%2C%22sortingSetting%22%3A%22CHRONOLOGICAL%22%2C%22stream_initial_count%22%3A1%2C%22useDefaultActor%22%3Afalse%2C%22id%22%3A%22314810416899010%22%2C%22__relay_internal__pv__GHLShouldChangeAdIdFieldNamerelayprovider%22%3Afalse%2C%22__relay_internal__pv__GHLShouldChangeSponsoredDataFieldNamerelayprovider%22%3Afalse%2C%22__relay_internal__pv__IsWorkUserrelayprovider%22%3Afalse%2C%22__relay_internal__pv__CometFeedStoryDynamicResolutionPhotoAttachmentRenderer_experimentWidthrelayprovider%22%3A500%2C%22__relay_internal__pv__CometImmersivePhotoCanUserDisable3DMotionrelayprovider%22%3Afalse%2C%22__relay_internal__pv__IsMergQAPollsrelayprovider%22%3Afalse%2C%22__relay_internal__pv__FBReelsMediaFooter_comet_enable_reels_ads_gkrelayprovider%22%3Afalse%2C%22__relay_internal__pv__CometUFIReactionsEnableShortNamerelayprovider%22%3Afalse%2C%22__relay_internal__pv__CometUFIShareActionMigrationrelayprovider%22%3Atrue%2C%22__relay_internal__pv__StoriesArmadilloReplyEnabledrelayprovider%22%3Atrue%2C%22__relay_internal__pv__EventCometCardImage_prefetchEventImagerelayprovider%22%3Afalse%7D&server_timestamps=true&doc_id=8710004115744333`;
 
-// Define headers for POST requests
 const POST_HEADERS = {
     'Accept': '*/*',
     'Accept-Language': 'vi,en;q=0.9',
@@ -56,11 +53,10 @@ const GET_HEADERS = {
     'Viewport-Width': '1446'
 };
 
-// MongoDB connection URI and client setup
-const MONGODB_URI = 'mongodb://localhost:27017'; // Replace with your MongoDB URI
+const MONGODB_URI = 'mongodb://localhost:27017';
 const client = new MongoClient(MONGODB_URI);
 
-// Function to connect to MongoDB
+
 async function connectToMongo() {
     try {
         await client.connect();
@@ -70,7 +66,6 @@ async function connectToMongo() {
     }
 }
 
-// Function to get the cursor from the initial POST request
 async function getCursor() {
     try {
         const response = await axios.post(URL, INITIAL_PAYLOAD, { headers: POST_HEADERS });
@@ -84,7 +79,6 @@ async function getCursor() {
     }
 }
 
-// Function to get data from a permalink URL
 async function getData(permalinkUrl) {
     try {
         const response = await axios.get(permalinkUrl, { headers: GET_HEADERS });
@@ -165,11 +159,7 @@ async function getPage() {
         // Iterate over unique links and fetch data
         for (const link of uniqueLinks) {
             const data = await getData(link);
-            // You can store `data` to MongoDB here if needed
-            // Example:
-            // const db = client.db('your_database');
-            // const collection = db.collection('your_collection');
-            // await collection.insertOne(data);
+
         }
     } catch (error) {
         console.error('Error in getPage:', error);
